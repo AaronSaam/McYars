910c656f4a7fff9076143253b9fa445705aa3d8e
Cookierock <aaronsaam@live.nl>
2014-02-16 13:14:21 +0100

    Kompastest #3 + Layout

--- www/js/main.js
+++ www/js/main.js
@@ -6,9 +6,15 @@ function getLocation() {
 function onSuccess(position) {
         document.getElementById("actionbar").style.background = "green"
 		var element = document.getElementById('geolocation');
-		document.getElementById('kompasrichting').innerHTML = position.coords.heading;
-		document.getElementById('kompas').style.transform = "rotate(" + position.coords.position + "deg)";
-}
+		alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp  	     + '\n');
+};
 
 function onError(error) {
     document.getElementById("actionbar").style.background = "red"
