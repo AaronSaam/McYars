fb8f007811a0ef1236ec270bb80ac2cc5a8d486c
Cookierock <aaronsaam@live.nl>
2014-02-28 00:11:54 +0100

    v0.0.2
    + Laden foutcheck
    + Ordening code
    + Meer javascript, minder html

---
+++ www/js/load.js
@@ -0,0 +1,30 @@
+var bol_wijzer = new Boolean(true);
+var bol_locatie = new Boolean(true);
+
+function load_actionbar() {
+    document.getElementById('button_locatie').src = "img/ic_action_location_searching.png";
+    document.getElementById('button_locatie').style.visibility = 'visible';
+
+    navigator.geolocation.getCurrentPosition(locatie_succes, load_error_locatie);
+
+    if (bol_locatie = true) {
+        heading = position.coords.heading;
+        if (heading == 'NaN' || heading == null) {
+            bol_wijzer = false;
+        };
+
+        document.getElementById('load_error_locatie').style.visibility = 'hidden';
+    };
+};
+
+function load_error_locatie(error) {
+    bol_locatie = false;
+    document.getElementById('load_error_locatie').style.visibility = 'visible';
+    document.getElementById('button_locatie').src = "img/ic_action_location_off.png";
+    document.getElementById('load_error_locatie_code').innerHTML = 'ERROR ' + error.code + '\n' + error.message + '\n';
+};
+
+function load_error_locatie_button() {
+    document.getElementById('load_error_locatie').style.visibility = 'hidden';
+    load_actionbar();
+};
--- www/js/main.js
+++ www/js/main.js
@@ -1,23 +1,7 @@
-// Variabelen
-var geenKompasWijzer = new Boolean(0);
-var geenLocatie = new Boolean(0);
-
-function onLoad() {
-
-document.addEventListener("deviceready", getLocation, false);
-}
-if (geenLocatie == false) {
-
-    function getLocation() {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-        document.getElementById('locatieknop').src = "img/ic_action_location_searching.png";
-    }
-
-};
-
-function onSuccess(position) {
-    document.getElementById('locatieknop').src = "img/ic_action_location_found.png";
-    alert('Latitude: ' + position.coords.latitude + '\n' +
+function locatie_succes(position) {
+    document.getElementById('button_locatie').src = "img/ic_action_location_found.png";
+    
+	alert('Latitude: ' + position.coords.latitude + '\n' +
         'Longitude: ' + position.coords.longitude + '\n' +
         'Altitude: ' + position.coords.altitude + '\n' +
         'Accuracy: ' + position.coords.accuracy + '\n' +
@@ -26,28 +10,10 @@ function onSuccess(position) {
         'Speed: ' + position.coords.speed + '\n' +
         'Timestamp: ' + position.timestamp + '\n');
 
-    if (geenKompasWijzer == false) {
-        var heading = position.coords.heading;
-        if (heading = 'NaN' || heading == null) {
-            geenKompasWijzer == true;
-            alerticon();
-        } else {
-            geenKompasWijzer == false;
-        };
-    };
 };
 
-function alerticon() {
-    document.getElementById('alert').style.visibility = 'visible';
-}
-
-function viewAlert() {
-    alert('We kunnen je richting niet bepalen.' + '\n' + 'Je apparaat bevat geen magneten.');
-};
-
-// Fout bij ophalen locatie
-function onError(error) {
-    document.getElementById('locatieknop').src = "img/ic_action_location_off.png";
+function locatie_error(error) {
+    document.getElementById('button_locatie').src = "img/ic_action_location_off.png";
     alert('code: ' + error.code + '\n' +
         'message: ' + error.message + '\n');
 };
