6638909b9638fc61357ed4cecc6a0cac52f5cc7e
Cookierock <aaronsaam@live.nl>
2014-02-28 22:00:28 +0100

    Startcontrole

--- www/js/load.js
+++ www/js/load.js
@@ -1,20 +1,27 @@
 var bol_wijzer = new Boolean(true);
 var bol_locatie = new Boolean(true);
+var page
 
 function load_actionbar() {
     document.getElementById('button_locatie').src = "img/ic_action_location_searching.png";
     document.getElementById('button_locatie').style.visibility = 'visible';
 
-    navigator.geolocation.getCurrentPosition(locatie_succes, load_error_locatie);
+    navigator.geolocation.getCurrentPosition(load_succes_locatie, load_error_locatie);
 
-    if (bol_locatie = true) {
-        heading = position.coords.heading;
-        if (heading == 'NaN' || heading == null) {
-            bol_wijzer = false;
-        };
+    
+};
 
-        document.getElementById('load_error_locatie').style.visibility = 'hidden';
+
+function load_succes_locatie(position) {
+    document.getElementById('button_locatie').src = "img/ic_action_location_found.png";
+            
+	heading = position.coords.heading;
+    if (heading == 'NaN' || heading == null) {
+    	bol_wijzer = false;
     };
+		
+	document.getElementById('load_error_locatie').style.visibility = 'hidden';
+	document.getElementById('kompaspage').style.visibility = 'visible';
 };
 
 function load_error_locatie(error) {
@@ -25,6 +32,5 @@ function load_error_locatie(error) {
 };
 
 function load_error_locatie_button() {
-    document.getElementById('load_error_locatie').style.visibility = 'hidden';
     load_actionbar();
 };
