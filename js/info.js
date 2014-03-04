ea598b418b27c9d9bd281f078787d9e62afeccee
Cookierock <aaronsaam@live.nl>
2014-03-04 15:25:31 +0100

    Info toegevoegd

---
+++ js/info.js
@@ -0,0 +1,5 @@
+plek_aanwezig = 1;
+
+function plek_info(plek_aanwezig, plek) {
+	document.getElementById('plek_naam').innerHTML = plek(1).naam;
+}
--- js/kompas.js
+++ js/kompas.js
@@ -3,9 +3,11 @@ var bol_locatie = new Boolean(false);
 
 var plek = TAFFY([{
     naam: 'Munsterkerk',
+    adres: 'Munsterplein',
+    Architect: 'Pierre Cuypers, Jos Hermans',
     N: '51.113700',
     O: '5.591900',
-    adres: 'Munsterplein'
+
 }, {
     naam: 'Sint-Christoffelkathedraal',
     N: '51.114725',
@@ -18,12 +20,12 @@ var randomnumber=Math.floor(Math.random()*plek().count()+1);
 alert(randomnumber) };*/
 
 function getLocation() {
-	navigator.geolocation.watchPosition(onSuccess, onError);
+    navigator.geolocation.watchPosition(onSuccess, onError);
 };
 
 function onSuccess(position) {
     document.getElementById('button_locatie').src = "img/ic_action_location_found.png";
-    document.getElementById('footer_accuracy').innerHTML = position.coords.accuracy + '%';
+    document.getElementById('footer_accuracy').innerHTML = Math.round(position.coords.accuracy) + '%';
 
     if (bol_locatie == false) {
         bol_locatie = true;
@@ -34,13 +36,13 @@ function onSuccess(position) {
         heading = position.coords.heading;
         if (isNaN(heading) || heading == null) {
             document.getElementById('wijzer').style.visibility = 'hidden';
-			document.getElementById('footer_error').innerHTML = "Richting niet ondersteund";
+            document.getElementById('footer_error').innerHTML = "Richting niet ondersteund";
             bol_wijzer = false;
         };
-	
-	if (bol_wijzer == true) {
-		document.getElementById('wijzer').style = '-webkit-transform: rotate(' + position.coords.heading + 'deg); -o-transform: rotate(' + position.coords.heading + 'deg); transform: rotate(' + position.coords.heading + 'deg); -moz-transform: rotate(' + position.coords.heading + 'deg); -ms-transform: rotate(' + position.coords.heading + 'deg)';	
-	};
+
+        if (bol_wijzer == true) {
+            document.getElementById('wijzer').style = '-webkit-transform: rotate(' + position.coords.heading + 'deg); -o-transform: rotate(' + position.coords.heading + 'deg); transform: rotate(' + position.coords.heading + 'deg); -moz-transform: rotate(' + position.coords.heading + 'deg); -ms-transform: rotate(' + position.coords.heading + 'deg)';
+        };
     };
 }
 
