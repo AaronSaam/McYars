6d89bc66ad25f7ce20dca87db12e48e408844f15
Cookierock <aaronsaam@live.nl>
2014-03-05 14:12:33 +0100

    Experimenteren

--- js/info.js
+++
@@ -1,5 +0,0 @@
-plek_aanwezig = 1;
-
-function plek_info(plek_aanwezig, plek) {
-	document.getElementById('plek_naam').innerHTML = plek(1).naam;
-}
--- js/main.js
+++ js/main.js
@@ -1,26 +1,69 @@
-var bol_wijzer = 1;
+var bol_wijzer = new Boolean(true);
 var bol_locatie = new Boolean(false);
+var plek_keuze
 
-// LOAD
-function load_actionbar() {
-    document.getElementById('button_locatie').src = "img/ic_action_location_searching.png";
-    document.getElementById('button_locatie').style.visibility = 'visible';
+var plek = [['Munsterkerk','Munsterplein','Pierre Cuypers' + '\n' + 'Jos Hermans','51.113700','5.591900']['Sint-Christoffelkathedraal','Kraanpoort','Pierre Cuypers','51.114725','5.590401']];
 
-    navigator.geolocation.getCurrentPosition(load_succes_locatie, load_error_locatie);  
+// Hoofdmenu
+arr_keuze = [-1, -1];
+
+function keuze(plek_keuze) {
+	arr_keuze[1] = plek_keuze
+	document.getElementById('k'+arr_keuze[0]).style.background = 'white';
+	document.getElementById('k'+arr_keuze[0]).style.color = 'black';
+	document.getElementById('k'+arr_keuze[1]).style.background = 'royalblue';
+	document.getElementById('k'+arr_keuze[1]).style.color = 'white';
+	arr_keuze[0] = arr_keuze[1];
+	if (plek_keuze == -1) {
+		plek_keuze=Math.floor(Math.random()*(plek().count()+1));
+	}
+}
+
+// Kompas
+function getLocation() {
+    navigator.geolocation.watchPosition(onSuccess, onError);
 };
 
-
-function load_succes_locatie(position) {
+function onSuccess(position) {
     document.getElementById('button_locatie').src = "img/ic_action_location_found.png";
-            
-	heading = position.coords.heading;
-    if (isNaN(heading) || heading == null) {
-    	bol_wijzer = 2;
+    document.getElementById('footer_accuracy').innerHTML = Math.round(position.coords.accuracy) + '%';
+
+    if (bol_locatie == false) {
+        bol_locatie = true;
+        document.getElementById('kompasbg').src = "img/KompasBlauw.png";
+    };
+
+    if (bol_wijzer == true) {
+        heading = position.coords.heading;
+        if (isNaN(heading) || heading == null) {
+            document.getElementById('wijzer').style.visibility = 'hidden';
+            document.getElementById('footer_error').innerHTML = "Richting niet ondersteund";
+            bol_wijzer = false;
+        };
+
+		heading = 50;
+		document.getElementById('afstand').innerHTML = heading;
+        if (bol_wijzer == true) {
+			document.getElementById('wijzer').style.visibility = 'visible';
+            document.getElementById('wijzer').style = '-webkit-transform: rotate(' +  + 'deg); -o-transform: rotate(' + heading + 'deg); transform: rotate(' + heading + 'deg); -moz-transform: rotate(' + heading + 'deg); -ms-transform: rotate(' + heading + 'deg)';
+        };
+    };
+}
+
+function onError(error) {
+    document.getElementById('button_locatie').src = "img/ic_action_location_off.png";
+    document.getElementById('footer_accuracy').innerHTML = 'Error';
+    document.getElementById('footer_error').innerHTML = '#' + error.code + ' - ' + error.message;
+    if (bol_locatie == true) {
+        bol_locatie = false;
+        document.getElementById('kompasbg').src = "img/KompasGrijs.png";
     };
 };
 
-function load_error_locatie(error) {
-    bol_locatie = false;
-    document.getElementById('button_locatie').src = "img/ic_action_location_off.png";
-    alert('ERROR ' + error.code + '\n' + error.message);
-};
+//INFORMATIE
+plek_aanwezig = 1;
+
+function plek_info(plek_aanwezig) {
+
+document.getElementById('plek_naam').innerHTML = plek[1][0];
+}
