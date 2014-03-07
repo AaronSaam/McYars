f4c2b251fbeb980518991e1e86eb06a294547666
Cookierock <aaronsaam@live.nl>
2014-03-07 18:07:01 +0100

    Bugfix?

--- js/index.js
+++ js/index.js
@@ -3,18 +3,21 @@ var lat_bestemming;
 var long_bestemming;
 
 var plek = [
-['Munsterkerk','Munsterplein','Pierre Cuypers + Jos Hermans','51.113700','5.591900'],
-['Sint-Christoffelkathedraal','Kraanpoort','Pierre Cuypers','51.114725','5.590401'] ];
+['Munsterkerk',51.193611, 5.988611,'Munsterplein','Pierre Cuypers + Jos Hermans'],
+['Sint-Christoffelkathedraal', 51.196458, 5.984447,'Kraanpoort','Pierre Cuypers'] ];
 
 function keuze(bestemming) {
-	lat_bestemming = plek[bestemming][3];
-	long_bestemming = plek[bestemming][4];
+	lat_bestemming = plek[bestemming][1];
+	long_bestemming = plek[bestemming][2];
 	navigator.geolocation.watchPosition(getLocatie, errorLocatie, {frequency: 100, timeout: 5000, enableHighAccuracy: true});
 }
 
 function getLocatie(position) {
 	document.getElementById('kompasbg').src = "img/KompasBlauw.png";
-	var hoek = (position.coords.longitude - long_bestemming) /(position.coords.latitude - lat_bestemming);
+	
+	var long = position.coords.longitude;
+	var lat = position.coords.latitude;
+	var hoek = (long_bestemming - long) / (lat_bestemming - lat);
 	var hoek_bestemming = Math.atan(hoek *(180/Math.PI));
 	var draaihoek = position.coords.heading + hoek_bestemming;
 	$('#wijzer').css('-webkit-transform', 'rotate(' + draaihoek + 'deg)');
