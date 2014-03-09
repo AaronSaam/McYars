8538f83b321b8d52c44978469d5e710dd5a7e3a4
Cookierock <aaronsaam@live.nl>
2014-03-09 21:01:03 +0100

    Externe interne database

--- js/index.js
+++ js/index.js
@@ -1,31 +1,9 @@
 var bestemming_lat;
 var bestemming_long;
-
-var plek = [
-['Munsterkerk',					51.193611, 5.988611, 'Munsterplein',			'Pierre Cuypers, Jos Hermans'],
-['Sint-Christoffelkathedraal',	51.196458, 5.984447, 'Kraanpoort',				'Pierre Cuypers'],
-['Minderbroederskerk',			51.191492, 5.986339, 'Minderbroedersstraat',	'Pierre Cuypers'],
-['Stadhuis',					51.195889, 5.985847, 'Markt 31',				'Pierre Cuypers'],
-['De Kiosk',					51.193353, 5.988031, 'Munsterplein',			'Pierre Cuypers'],
-['Ursulakapel',					51.195614, 5.990875, 'Voogdijstraat 24',		'Jos Cuypers'],
-['Caroluskapel',				51.197033, 5.989219, 'Swalmerstraat 100',		'Jos Hermans'],
-['Steenen Trappen',				51.193339, 5.985681, 'Neerstraat 33',			'Peter van Boshuysen'],
-['Nationaal Indië-monument',	51.181178, 5.987481, 'Maastrichterweg 25',		'Dick van Wijk, Wijnand Thönissen']
-/*['Rattentoren',
-['Synagoge',
-['Bisschoppelijk Paleis',
-['Historiehuis',
-['Wegkruis met houten corpus',
-['Pierre Cuypers',
-['Jubileumfontein',
-['De Ruivers',
-['Emile Seipgens',
-['Schinderhannes',
-['Cuypershuis',
-['Teeken en ambachtschool',*/
-];
+var bestemming_keuze;
 
 function keuze(bestemming) {
+	bestemming_keuze = bestemming;
 	bestemming_lat = plek[bestemming][1];
 	bestemming_long = plek[bestemming][2];
 	navigator.geolocation.watchPosition(getLocatie, errorLocatie, {frequency: 100, timeout: 5000, enableHighAccuracy: true});
@@ -33,23 +11,10 @@ function keuze(bestemming) {
 
 function getLocatie(position) {
 	document.getElementById('kompasbg').src = "img/KompasBlauw.png";
-	var dlong;
-	var dlat;
+	var dlong = bestemming_long - position.coords.longitude;
+	var dlat = bestemming_lat - position.coords.latitude;	
 	var hoek;
-	if (position.coords.longitude < 0) {
-		dlong = bestemming_long + position.coords.longitude;
-	}
-	else {
-		dlong = bestemming_long - position.coords.longitude;
-	}
 	
-	if (position.coords.latitude < 0) {
-		dlat = bestemming_lat + position.coords.latitude;
-	}
-	else {
-		dlat = bestemming_lat - position.coords.latitude;	
-	}
-		
 	if (dlong == 0) {
 		hoek = 0;
 	}
---
+++ js/plek.js
@@ -0,0 +1,210 @@
+var plek = [
+//1
+['Munsterkerk',
+51.193611,
+5.988611,
+'Munsterplein',
+'13e eeuw',
+'Pierre Cuypers, Jos Hermans',
+'De enige kerk in Nederland met een laat-Romaanse bouwstijl. De Munsterkerk bevat naast zijn prachtige bouwstijl ook het praalgraf van zijn stichters (Gerard van Gelre en zijn vrouw Margaretha van Brabant).'
+],
+
+//2
+['Sint-Christoffelkathedraal',
+51.196458,
+5.984447,
+'Kraanpoort',
+'15e eeuw',
+'Pierre Cuypers',
+'De Sint-Christoffelkathedraal is de hoofdkerk van de bisdom Roermond, een van de 7 bisdoms in Nederland. De kerk stamt uit de 15e eeuw met een Gotische bouwstijl.'
+],
+
+//3
+['Minderbroederskerk',
+51.191492,
+5.986339,
+'Minderbroedersstraat',
+'15e eeuw',
+'Pierre Cuypers',
+'De Minderbroederskerk is een Protestantse kerk uit de 14e eeuw. In 1307 stichtte de Orde der Minderbroeders een klooster. De huidige kerk werd nabij pas in de 15e eeuw gebouwd.'
+],
+
+//4
+['Stadhuis',
+51.195889,
+5.985847,
+'Markt 31',
+'18e eeuw',
+'Pierre Cuypers',
+'Het stadhuis kent een lange geschiedenis. Rond de 18e eeuw was het een vergaderplaats voor de staten van Gelre. Tegenwoordig moet het dienen als centrum van stedelijk rechtspraak en bestuur.'
+],
+
+//5
+['De Kiosk',
+51.193353,
+5.988031,
+'Munsterplein',
+'19e eeuw',
+'Pierre Cuypers',
+'De Kiosk is ontworpen door Pierre Cuypers. Tegenwoordig spelen veel harmonieorkesten in de Kiosk.'
+],
+
+//6
+['Ursulakapel',
+51.195614,
+5.990875,
+'Voogdijstraat 24',
+'20e eeuw',
+'Jos Cuypers',
+'De Ursulakapel is het voormalige kloosterkapel van het inmiddels afgebroken St.-Urselaklooster. De kapel is gebouwd door Jos Cuypers.'
+],
+
+//7
+['Caroluskapel',
+51.197033,
+5.989219,
+'Swalmerstraat 100',
+'14e eeuw',
+'Jos Hermans',
+'Laatgotische kapel van het voormalige Karthuizerklooster, met een Lodewijk XV stijl.'
+],
+
+//8
+['Steenen Trappen',
+51.193339,
+5.985681,
+'Neerstraat 33',
+'17e eeuw',
+'Peter van Boshuysen',
+'In 1666 werd het gebouwd voor een ambt uit Montfort. Het huis kent een Classicistische stijl, Tegenwoordig wordt het gebruikt als kantorenpand.'],
+
+//9
+['Nationaal Indië-monument',
+51.181178,
+5.987481,
+'Maastrichterweg 25',
+'20e eeuw',
+'Dick van Wijk, Wijnand Thönissen',
+'Monument ter nagedachtenis van de Nederlandse militairen die in de periode tussen 1945 en 1962 in het voormalige Nederlands-Indië of in Nieuw-Guinea vielen.'
+],
+
+//10
+['Rattentoren',
+51.196989,
+5.984286,
+'Grote kerkstraat 29',
+'14e eeuw',
+'Louis Scheepers, Pierre Cuypers',
+'Bij de kathedraal staat een restant uit de late-Middeleeuwen. Deze muurtoren is de enige overgebleven toren van de vestigingsstad Roermond.'
+],
+
+//11
+['Synagoge',
+51.192981,
+5.990883,
+'Hamstraat 20',
+'19e eeuw',
+'A. Oznowicz',
+'De enige Joodse gebedsplaats in het hart van Roermond, met een rijke historie.'
+],
+
+//12
+['Bisschoppelijk Paleis',
+51.193200,
+5.986753,
+'Paredisstraat 10',
+'19e eeuw',
+'Caspar Franssen',
+'Rond 1860 sticht Bisschop Paredis een nieuw Bisschoppelijk Paleis, net voor de Tweede Wereldoorlog verbouwde architect Caspar Franssen het Exterieur.'
+],
+
+//13
+['Historiehuis',
+51.194275,
+5.985728,
+'Neerstraat 11-13',
+'21e eeuw',
+'Maurice Mentjens',
+'In het Historiehuis maak je kennis met de geschiedenis van Roermond, met de nadruk op het leven in Roermond tijdens de Middeleeuwen.'
+],
+
+//14
+['Wegkruis met houten corpus',
+51.191667,
+5.986111,
+'Minderbroedersstraat 4',
+'17e eeuw',
+'Jozef Thissen',
+'Een prachtig gedenkteken van Jezus Christus aan het kruis.'
+],
+
+//15
+['Pierre Cuypers',
+51.193514,
+5.988703,
+'Munsterplein',
+'20e eeuw',
+'August Falise',
+'Een bronzen beeld van de Roermondse architect Pierre Cuypers.'
+],
+
+//16
+['Jubileumfontein',
+51.193536,
+5.989461,
+'Munsterplein',
+'20e eeuw',
+'Dolf Wong Hun Ling',
+'Een Jubileumfontein die na de Tweede Wereldoorlog is aangelegd.'
+],
+
+//17
+['De Ruivers',
+51.194494,
+5.985319,
+'Neerstraat',
+'20e eeuw',
+'Dick van Wijk, Wijnand Thönissen',
+'Bronzen beeld van een aantal rovers op een betonnen zuil.'
+],
+
+//18
+['Emile Seipgens',
+51.194606,
+5.984083,
+'Burgstraat (Roerkade)',
+'20e eeuw',
+'Dick van Wijk',
+'Bronzen beeld van de Nederlandse schrijver Emile Seipgens.'
+],
+
+//19
+['Schinderhannes',
+51.194828,
+5.986494,
+'Bergstraat',
+'20e eeuw',
+'Dolf Wong Hun Ling',
+'Bronzen beeld van de Duitse rover Johannes Bückler op zijn paard.'
+],
+
+//20
+['Cuypershuis',
+51.188056,
+5.988333,
+'Pierre Cuypersstraat 1',
+'19e eeuw',
+'Pierre Cuypers',
+'Het Cuypershuis is hét museum over de Roermondse architect/ontwerper Pierre Cuypers.'
+],
+
+//21
+['Teeken en ambachtschool',
+51.195278,
+5.993611,
+'Godsweerderstraat 2',
+'20e eeuw',
+'J. van Lokhorst, L.H. Luijten, Jos Cuypers',
+'Teeken- en ambachtschool voor wetenschap en onderwijs.'
+]
+];
